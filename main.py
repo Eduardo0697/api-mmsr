1b4bea9b16f74b30d0087c57f8551d65c57c40f8
Eduardo Hernandez <eduardohdz0697@hotmail.com>
2023-01-16 09:38:26 +0100

    Add CORS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from enum import Enum
 from typing import Union
 from fastapi import FastAPI
 import json
+from fastapi.middleware.cors import CORSMiddleware
 
 # Imports for project
 import pandas as pd
@@ -44,6 +45,23 @@ topIdsFiles = {
 }
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "https://mmsr-app.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
