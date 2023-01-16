2fd8364ac1fb1dfbe08906b7b00eb68681939515
Eduardo Hernandez <eduardohdz0697@hotmail.com>
2023-01-16 01:36:56 +0100

    heroku one model

--- files.py
+++ files.py
@@ -3,32 +3,3 @@
 file_genres   = "./data/id_genres_mmsr.tsv"
 file_info    = "./data/id_information_mmsr.tsv"
 file_urls    = './data/id_url_mmsr.tsv'
-
-# Model Datasets
-
-# Early Fusion Models Cosine and Jaccard
-f_top_cosine_bert_blf_spectral_resnet = "./data/top_ids_cosine_earlyfusion_bert_blf_spectral_resnet_complete.csv"
-f_top_cosine_bert_blf_spectral_incp   = "./data/top_ids_cosine_earlyfusion_bert_blf_spectral_incp_complete.csv"
-f_top_cosine_bert_mfcc_bow_resnet     = "./data/top_ids_cosine_earlyfusion_bert_mfcc_bow_resnet_complete.csv"
-f_top_cosine_bert_mfcc_bow_incp       = "./data/top_ids_cosine_earlyfusion_bert_mfcc_bow_incp_complete.csv"
-f_top_cosine_bert_blf_logfluc_resnet  = "./data/top_ids_cosine_earlyfusion_bert_blf_logfluc_resnet_complete.csv"
-f_top_cosine_bert_blf_logfluc_incp    = "./data/top_ids_cosine_earlyfusion_bert_blf_logfluc_incp_complete.csv"
-f_top_cosine_tfidf_blf_spectral_resnet= "./data/top_ids_cosine_earlyfusion_tfidf_blf_spectral_resnet_complete.csv"
-f_top_cosine_tfidf_blf_spectral_incp  = "./data/top_ids_cosine_earlyfusion_tfidf_blf_spectral_incp_complete.csv"
-f_top_cosine_tfidf_mfcc_bow_resnet    = "./data/top_ids_cosine_earlyfusion_tfidf_mfcc_bow_resnet_complete.csv"
-f_top_cosine_tfidf_mfcc_bow_incp      = "./data/top_ids_cosine_earlyfusion_tfidf_mfcc_bow_incp_complete.csv"
-f_top_cosine_tfidf_blf_logfluc_resnet = "./data/top_ids_cosine_earlyfusion_tfidf_blf_logfluc_resnet_complete.csv"
-f_top_cosine_tfidf_blf_logfluc_incp   = "./data/top_ids_cosine_earlyfusion_tfidf_blf_logfluc_incp_complete.csv"
-
-f_top_jaccard_bert_essentia_vgg19      = "./data/top_ids_jaccard_earlyfusion_bert_essentia_vgg19_complete.csv"
-f_top_jaccard_bert_essentia_resnet     = "./data/top_ids_jaccard_earlyfusion_bert_essentia_resnet_complete.csv"
-f_top_jaccard_bert_blf_logfluc_vgg19   = "./data/top_ids_jaccard_earlyfusion_bert_blf_logfluc_vgg19_complete.csv"
-f_top_jaccard_bert_blf_logfluc_resnet  = "./data/top_ids_jaccard_earlyfusion_bert_blf_logfluc_resnet_complete.csv"
-f_top_jaccard_bert_mfcc_stats_vgg19    = "./data/top_ids_jaccard_earlyfusion_bert_mfcc_stats_vgg19_complete.csv"
-f_top_jaccard_bert_mfcc_stats_resnet   = "./data/top_ids_jaccard_earlyfusion_bert_mfcc_stats_resnet_complete.csv"
-f_top_jaccard_tfidf_essentia_vgg19     = "./data/top_ids_jaccard_earlyfusion_tfidf_essentia_vgg19_complete.csv"
-f_top_jaccard_tfidf_essentia_resnet    = "./data/top_ids_jaccard_earlyfusion_tfidf_essentia_resnet_complete.csv"
-f_top_jaccard_tfidf_blf_logfluc_vgg19  = "./data/top_ids_jaccard_earlyfusion_tfidf_blf_logfluc_vgg19_complete.csv"
-f_top_jaccard_tfidf_blf_logfluc_resnet = "./data/top_ids_jaccard_earlyfusion_tfidf_blf_logfluc_resnet_complete.csv"
-f_top_jaccard_tfidf_mfcc_stats_vgg19   = "./data/top_ids_jaccard_earlyfusion_tfidf_mfcc_stats_vgg19_complete.csv"
-f_top_jaccard_tfidf_mfcc_stats_resnet  = "./data/top_ids_jaccard_earlyfusion_tfidf_mfcc_stats_resnet_complete.csv"
